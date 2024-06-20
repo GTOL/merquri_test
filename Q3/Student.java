61cbc051ff5d8793d35c4817ac01f349555e0116
GTOL <gt531593545@126.com>
2024-06-20 14:18:42 +0800

    Create Student.java

---
+++ Q3/Student.java
@@ -0,0 +1,8 @@
+class Student {
+    public String name;
+    public String gender;
+    public Student(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+}
